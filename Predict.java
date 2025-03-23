047fd109aeb1a3f706a2f5d6b81cd053f84fef8f
DiegoBrando1325 <diegomercado_12@outlook.com>
2025-03-23 17:58:10 -0600

    Métodos declarados en Predict

--- Predict.java
+++ Predict.java
@@ -1,2 +1,14 @@
 public class Predict {
+    private void readFile(){
+    }
+
+    private void writeFile(){
+    }
+
+    public void decryptByBrutForce(){
+    }
+
+    public void decryptByAnalEst(){
+    }
+
 }
