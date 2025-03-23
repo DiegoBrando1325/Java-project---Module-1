24ec1fdb837b8c9f0b7b43b20a03d7fb06edf88b
DiegoBrando1325 <diegomercado_12@outlook.com>
2025-03-23 16:58:50 -0600

    Métodos declarados en MainApp

--- MainApp.java
+++ MainApp.java
@@ -1,2 +1,21 @@
 public class MainApp {
+
+    public void configKey(){
+    }
+
+    public void encrypt(){
+    }
+
+    public void decrypt(){
+    }
+
+    public void brutForce(){
+    }
+
+    public void analEst(){
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
